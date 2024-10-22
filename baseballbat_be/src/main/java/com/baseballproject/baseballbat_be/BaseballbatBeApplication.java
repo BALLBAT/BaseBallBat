dfd220c195323b27354f1d020d3fca18a661a0eb
BALLBAT <159212526+BALLBAT@users.noreply.github.com>
2024-10-23 00:21:43 +0900

    2024.10.23
    1. 백엔드 프로젝트 생성

---
+++ baseballbat_be/src/main/java/com/baseballproject/baseballbat_be/BaseballbatBeApplication.java
@@ -0,0 +1,13 @@
+package com.baseballproject.baseballbat_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballbatBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseballbatBeApplication.class, args);
+	}
+
+}
